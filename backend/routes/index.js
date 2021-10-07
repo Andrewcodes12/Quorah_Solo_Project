c78545a3b55ec81dc9e91a4d3e50fec7ed25f4e8
Andrewcodes12 <AndrewFava@yahoo.com>
2021-10-07 12:58:41 -0400

    backend validators completed

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,9 +8,5 @@ router.use('/api', apiRouter);
 
 
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
 
 module.exports = router;
