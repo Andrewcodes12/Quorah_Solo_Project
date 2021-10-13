1986f1fe6a542b75ec09b04b4806578bf741db10
Andrewcodes12 <AndrewFava@yahoo.com>
2021-10-13 18:29:39 -0400

    delete is dynamic full crud completed for questions

--- frontend/src/components/Questions/index.js
+++ frontend/src/components/Questions/index.js
@@ -1,13 +1,17 @@
 import useToggle from '../../Hooks/useToggle';
 import { useState,useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector,useDispatch } from 'react-redux';
 import EditQuestionForm from '../EditQuestionForm';
 import { csrfFetch } from '../../store/csrf';
+import { removeQuestion } from '../../store/questions';
+import { useHistory } from 'react-router';
+
 import './questions.css'
 
 
 const QuestionsContainer = ({question}) => {
-
+const dispatch = useDispatch()
+const history = useHistory()
 
   const sessionUser = useSelector((state) => state.session.user);
 
@@ -15,16 +19,22 @@ const QuestionsContainer = ({question}) => {
   const [showForm, setShowForm] = useState(false);
 
 
+  const handleDelete = async (e) => {
+    e.preventDefault()
+    await dispatch(removeQuestion(question.id))
+    history.push(`/feed`)
+  }
+
+
       function deleteQuestion(questionId){
         csrfFetch(`/api/questions/${questionId}`,{
           method:'DELETE'
         }).then((result)=>{
           result.json().then((resp)=>{
-            console.log(resp)
+            console.log("deleted")
           })
         })
       }
-
 
   return (
         <div className="questionDiv">
@@ -40,7 +50,9 @@ const QuestionsContainer = ({question}) => {
             {question.Comments? <button className="commentBtn" onClick= {toggleComment}><i class="fas fa-comments"></i></button>: "" }
           {showComment && question.Comments?.map((comment) => <div> {comment.body} <button className="editComment">Edit Comment</button> <button className="deleteComment">Delete Comment</button> </div>)}
           <button className="addComment"> <i class="fas fa-plus-square"></i></button>
-          <button className="deleteQuestion" onClick={()=> deleteQuestion(question.id)}> <i class="fas fa-trash-alt"> </i></button>
+          <form onSubmit={handleDelete}>
+          <button className="deleteQuestion" type="submit"><i class="fas fa-trash-alt"> </i></button>
+        </form>
           </ul>
         </div>
 
