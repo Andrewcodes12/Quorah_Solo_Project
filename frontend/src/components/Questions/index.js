905aaeaa7a61dc4d084a24c862d8e8f719d8912e
Andrewcodes12 <AndrewFava@yahoo.com>
2021-10-13 17:33:25 -0400

    delete fetch pushed

--- frontend/src/components/Questions/index.js
+++ frontend/src/components/Questions/index.js
@@ -1,33 +1,47 @@
-import styles from './questions.css'
 import useToggle from '../../Hooks/useToggle';
-import { useState } from 'react';
-// import { useSelector } from 'react-redux';
+import { useState,useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import EditQuestionForm from '../EditQuestionForm';
+
+
+import './questions.css'
+
 
 const QuestionsContainer = ({question}) => {
 
 
-
+  const sessionUser = useSelector((state) => state.session.user);
 
   const [showComment, toggleComment] = useToggle("true")
   const [showForm, setShowForm] = useState(false);
+
+
+      function deleteQuestion(questionId){
+        fetch(`/delete/${questionId}`,{
+          method:'DELETE'
+        }).then((result)=>{
+          result.json().then((resp)=>{
+            console.log(resp)
+          })
+        })
+      }
+
 
   return (
         <div className="questionDiv">
           <div className="questionBody">
            {question.body}
            {showForm && <EditQuestionForm questionId={question.id} />}
-           <button className="editQuestionBtn" onClick={()=>setShowForm(true)}> Edit Question</button>
+           <button className="editQuestionBtn" onClick={()=>setShowForm(true)}><i class="fas fa-edit"></i></button>
           </div>
           {/* <div className="userId">
             {sessionUser.username}
           </div> */}
           <ul className="Comments">
-            {question.Comments? <button onClick= {toggleComment}>Show Comments</button>: "" }
+            {question.Comments? <button className="commentBtn" onClick= {toggleComment}><i class="fas fa-comments"></i></button>: "" }
           {showComment && question.Comments?.map((comment) => <div> {comment.body} <button className="editComment">Edit Comment</button> <button className="deleteComment">Delete Comment</button> </div>)}
-          <button> Add Comments</button>
-          {/* <button>Edit Question <EditQuestionForm /></button> */}
-          <button> Delete Question </button>
+          <button className="addComment"> <i class="fas fa-plus-square"></i></button>
+          <button className="deleteQuestion" onClick={()=> deleteQuestion(question.id)}> <i class="fas fa-trash-alt"> </i></button>
           </ul>
         </div>
 
